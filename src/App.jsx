a35c4544c058da989427f6ea32fef826dbeec6eb
fazalshah007 <bilalfazal52@gmail.com>
2025-01-26 17:01:19 +0500

    product card project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './tailwind.css'
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-green-400">
+        card project continue
+      </h1>
+    </>
+  )
+}
+
+export default App
